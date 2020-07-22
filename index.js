be9e38dd24547c81f3ec3aca35873619037e165c
aiody <aiody329@gmail.com>
2020-07-22 17:31:18 +0900

    FIX message when server failed to connect with DB.  - 데이터베이스에 접속이 실패했을 때 메시지가 뜨도록 수정하였습니다.

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ const port = 4000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-db.sequelize.authenticate().then(() => {
-  console.log('Connection has been established successfully.');
-});
+db.sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.log('Sequelize was unable to connect to the database:', err);
+  });
 
 app.listen(port, () => console.log(`AllGreenDay listening at port : ${port}`));
